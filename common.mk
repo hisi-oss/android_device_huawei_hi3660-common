ce9019e032407c3fa0af2aff35709220ffe4c96a
Giovanni Ricca <giovanniricca@proton.me>
2023-07-16 17:37:49 +0200

    hi3660-common: Borrow common overlays from hi6250-8
    Change-Id: Idff98453385dcd9a840e24fc12fbaee74b47cd47

--- common.mk
+++ common.mk
@@ -8,6 +8,9 @@
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Connectivity
