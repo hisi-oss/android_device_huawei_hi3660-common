718d3634b403a80dbd9cf849aceaf330a4b71c1b
A2L5E0X1 <alexed25@protonmail.com>
2024-01-30 19:30:45 +0100

    hi3660-common: Link `libhidlbase` against gralloc
    * dlopen failed: cannot locate symbol _ZN7android8hardware14IPCThreadState4selfEv referenced by /vendor/lib64/hw/gralloc.hi6250.so...
    
    Change-Id: I83bc59815efdbe007f6b07c3a5c3f989885e9a1a

--- extract-files.sh
+++ extract-files.sh
@@ -60,6 +60,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib*/hw/gralloc.hi3660.so)
+            "${PATCHELF}" --add-needed "libhidlbase.so" "${2}"
+            ;;
         vendor/lib*/hw/hwcomposer.hi3660.so)
             "${PATCHELF}" --replace-needed "libui.so" "libui-v28.so" "${2}"
             ;;
