96dafb02ebc7cf3f7ee661cb04f3e0961bdf1f46
Jan Altensen <info@stricted.net>
2024-01-31 10:36:58 +0100

    hi3660-common: Generate vulkan symlinks
    Change-Id: I25501e0ffcb86b8e21ee44c648b9f14bbb931b8b

--- Android.mk
+++ Android.mk
@@ -10,4 +10,24 @@ ifneq ($(filter stanford victoria,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+EGL_LIBS := hw/vulkan.hi3660.so
+
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
+
+EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(EGL_LIBS))
+$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 64 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
+
 endif
