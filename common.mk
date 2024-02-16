10065397060310dcc6f4fba92e6e29bbe891756e
Giovanni Ricca <giovanniricca@proton.me>
2024-02-16 11:22:45 +0100

    hi3660-common: Import initial init scripts
    * From STF-L09-user 9.1.0 HUAWEISTF-L09 231-OVS-LGRP2 release-keys
    
    Change-Id: I610587964d9268eecddd5e7e5ad4fc415975ec86

--- common.mk
+++ common.mk
@@ -4,8 +4,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Connectivity
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/etc/connectivity/,$(TARGET_COPY_OUT_VENDOR)/etc/init/connectivity)
+
+# Fstab
+PRODUCT_PACKAGES += \
+    fstab.hi3660 \
+    fstab.modem
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.audio.rc \
+    init.balong_modem.rc \
+    init.connectivity.rc \
+    init.device.rc \
+    init.hi3660.rc \
+    init.hisi.rc \
+    init.hisi.usb.rc \
+    init.platform.rc \
+    init.tee.rc \
+    init.vowifi.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/hisi \
     hardware/huawei
+
+# Ueventd
+PRODUCT_PACKAGES += \
+    ueventd.hi3660.rc
