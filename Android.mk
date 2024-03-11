b27ec92875ef7acbae2a76977271368393df0c09
Giovanni Ricca <giovanniricca@proton.me>
2024-03-11 13:44:22 +0100

    hi3660-common: Create `modem_fw` mountpoint
    Change-Id: Ic084904caf4e4ccd5cbc3481977478992449ab9b

--- Android.mk
+++ Android.mk
@@ -12,6 +12,13 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/modem/modem_fw
+$(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/modem/modem_fw
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT)
+
 EGL_LIBS := hw/vulkan.hi3660.so
 
 EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
